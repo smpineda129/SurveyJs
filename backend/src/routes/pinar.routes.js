5933258e37d7cde66971b675ab074b7e2fa9f618
LuisaMGuerrero <soporte.ti@gdinteligente.co>
2026-05-12 15:30:07 -0500

    apidocxII

--- backend/src/routes/pinar.routes.js
+++ backend/src/routes/pinar.routes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { generatePinarDocx } from "../controllers/pinar.controller.js";
 
+console.log("PINAR ROUTES CARGADAS");
+
 const router = express.Router();
 
 router.get("/generate-docx", (req, res) => {
@@ -10,3 +12,4 @@ router.get("/generate-docx", (req, res) => {
 router.post("/generate-docx", generatePinarDocx);
 
 export default router;
+
